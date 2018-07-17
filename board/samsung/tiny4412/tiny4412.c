27a5e6201a3f5f94ab8621f63f406deda6c953b0
lizuobin <271526143@qq.com>
2018-07-17 22:59:07 +0800

    4412:usb hub reset

--- board/samsung/tiny4412/tiny4412.c
+++ board/samsung/tiny4412/tiny4412.c
@@ -12,6 +12,9 @@
 #include <asm/arch/periph.h>
 #include <asm/arch/pinmux.h>
 #include <usb.h>
+#include <asm/arch/gpio.h>
+#include <asm/gpio.h>
+#include <asm/arch/clock.h>
 
 DECLARE_GLOBAL_DATA_PTR;
 
@@ -20,8 +23,21 @@ u32 get_board_rev(void)
 	return 0;
 }
 
+static void board_gpio_init(void)
+{
+    /* dm9621 reset Pin */
+    gpio_request(EXYNOS4X12_GPIO_M24, "DM9621 reset");
+
+    gpio_cfg_pin(EXYNOS4X12_GPIO_M24, S5P_GPIO_FUNC(0x1));
+    gpio_set_pull(EXYNOS4X12_GPIO_M24, S5P_GPIO_PULL_UP);
+    gpio_set_value(EXYNOS4X12_GPIO_M24, 0);
+    udelay(10000);
+    gpio_set_value(EXYNOS4X12_GPIO_M24, 1);
+}
+
 int exynos_init(void)
 {
+	board_gpio_init();
 	return 0;
 }
 
